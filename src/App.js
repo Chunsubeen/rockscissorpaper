bb07010693d998b4910d4577ef45c022464857c8
Subeen Chun <petitloutre@naver.com>
2024-08-15 06:50:35 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import {useState} from "react"
+import { useState } from "react";
 import './App.css';
-import Box from "./component/Box"
+import Box from "./component/Box";
 
-//1.박스 2개(타이틀,사진,결과값)
-//2.가위 바위 보 버튼 있음
-//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-//4.컴퓨터는 랜덤하게 아이템 선택이 된다
-//5.3,4번의 결과를 가지고 누가 이겼는지 승패를 따진다
-//6.승패 결과에 따라 테두리 색이 바뀐다(이기면-초록,지면-빨강,비기면-검은색)
 const choice = {
   rock: {
     name: "Rock",
-    img: "https://media.istockphoto.com/id/1183921035/vector/rock-sign-gesture-with-lightning-for-your-design.jpg?s=612x612&w=0&k=20&c=4r6LcLz2IQ_NBQ__YsSoy9rANKTvxwm-Bnw6lfoYNfM=",
+    img: "https://i.namu.wiki/i/G3RgLmpMXgENdo9TNMhpjFhgIT-pHMA6mmGeU0NWbrSjUfWivAeYsPxmvORYWOLF00hYlTHIgrAlhPO9Ed2CEQ.webp",
   },
   scissors: {
     name: "Scissors",
@@ -24,23 +18,51 @@ const choice = {
 };
 
 function App() {
-  const [userSelect,setUserSelect] = useState(null)
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [computerResult, setComputerResult] = useState("");
 
-  const play = (userChoice) =>{
-    setUserSelect(choice[userChoice])
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
   };
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    const userResult = judgement(choice[userChoice], computerChoice);
+    setResult(userResult);
+    setComputerResult(userResult === "win" ? "lose" : userResult === "lose" ? "win" : "tie");
+  };
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock") {
+      return computer.name === "Scissors" ? "win" : "lose";
+    } else if (user.name === "Scissors") {
+      return computer.name === "Paper" ? "win" : "lose";
+    } else if (user.name === "Paper") {
+      return computer.name === "Rock" ? "win" : "lose";
+    }
+  };
+
   return (
     <div>
-  <div className="main">
-    <Box title="You" item={userSelect}/>
-    {/* <Box title="Computer"/> */}
-  </div>
-  <div className="main">
-    <button onClick={() => play("scissors")}>가위</button>
-    <button onClick={() => play("rock")}>바위</button>
-    <button onClick={() => play("paper")}>보</button>
-  </div>
-  </div>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={computerResult} />
+      </div>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+    </div>
   );
 }
 
