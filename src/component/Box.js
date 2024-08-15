44e087bf5f805d4d6619ecfd7c10319a0cc8cdfe
Subeen Chun <petitloutre@naver.com>
2024-08-16 01:19:35 +0900

    first commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Box = (props) => {
-    return (
-        <div className={`box ${props.result}`}>
-            <h1>{props.title}</h1>
-            <img 
-            className="item-img"
-            src={props.item && props.item.img}/>
-            <h2>{props.result}</h2>
-        </div>
-    );
+class Box extends Component {
+    render() {
+        const { result, title, item } = this.props;
+
+        return (
+            <div className={`box ${result}`}>
+                <h1>{title}</h1>
+                <img 
+                    className="item-img"
+                    src={item && item.img}
+                    alt=""
+                />
+                <h2>{result}</h2>
+            </div>
+        );
+    }
 }
 
 export default Box;
